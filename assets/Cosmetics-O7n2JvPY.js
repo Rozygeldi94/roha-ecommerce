428cc7e55461c5325621cf433c3bd7d44992fd9b
Rozygeldi <rozygeldi1994@gmail.com>
2024-05-11 19:35:41 +0300

    Updates

--- assets/Cosmetics-O7n2JvPY.js
+++
@@ -1,1 +0,0 @@
-import{j as s,B as t}from"./index-DzQhS1Ae.js";import{u as i}from"./useDocumentTitle-CdATg2GL.js";function e(){return i("Cosmetics | RoHa"),s.jsx(t,{maxWidth:"1230px",width:"100%",margin:"30px auto",padding:{base:"0 10px",isLargerThan440:"0 15px"},children:"Cosmetics"})}export{e as default};
